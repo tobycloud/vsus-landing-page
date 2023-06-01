a4fde29ab39c365e5569d68b57a7b45915f29815
pdteggman <78996937+pdt1806@users.noreply.github.com>
2023-06-01 20:14:46 +0000

    add footer & theme toggle icons

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let darkmodeStatus = 0;
 function darkmode() {
     var button = document.getElementById('darkmodeButton');
-    button.classList.toggle('light-mode')
+    button.classList.toggle('light-mode');
     if (darkmodeStatus === 0){
-        button.innerText = 'Light Mode';
-        darkmodeStatus = 1
+        document.getElementById('darkmodeIcon').textContent = 'light_mode';
+        darkmodeStatus = 1;
     } else if (darkmodeStatus === 1) {
-        button.innerText = 'Dark Mode';
-        darkmodeStatus = 0
-    }
+        document.getElementById('darkmodeIcon').textContent = 'dark_mode';
+        darkmodeStatus = 0;
+    };
     document.body.classList.toggle("dark-mode");
 };
